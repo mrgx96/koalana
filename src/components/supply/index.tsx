139aa7e972dd8ff8ef1b3e70cc95bf60de167d23
dragon-8800 <159820590+dragon-8800@users.noreply.github.com>
2024-08-03 11:36:51 -0400

    :lipstick: fix: supply

--- src/components/supply/index.tsx
+++ src/components/supply/index.tsx
@@ -6,9 +6,9 @@ const Supply = () => {
     <section className={style.section}>
       <h3>Total Supply: 1 billion</h3>
       <div className={style.progressBars}>
-        <ProgressBar color="#D32B85" label="100% community token created on moonshot" value={50} />
+        <ProgressBar color="#D32B85" label="100% community token created on pump.fun" value={100} />
         <ProgressBar color="#FFAD00" label="150M-200M tokens burned for a deflationary boost" value={20} />
-        <ProgressBar color="#28b3b4" label="All liquidity is locked by burning the LP token" value={30} />
+        <ProgressBar color="#28b3b4" label="All liquidity is locked by burning the LP token" value={100} />
       </div>
     </section>
   );
