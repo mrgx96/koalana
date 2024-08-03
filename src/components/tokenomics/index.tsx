3bf88e6ef04ac5f5e1ffbd70025b76c930ae38f3
dragon-8800 <159820590+dragon-8800@users.noreply.github.com>
2024-08-03 11:51:19 -0400

    :lipstick: fix: remove koalanomics phrase

--- src/components/tokenomics/index.tsx
+++ src/components/tokenomics/index.tsx
@@ -6,10 +6,6 @@ const Tokenomics = () => {
   return (
     <section id="koalanomics" className={style.section}>
       <h2>Koalanomics</h2>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy.
-      </p>
       <div className={style.tokenomics}>
         <img src={tokenomics} className="img-responsive" />
         <Supply />
