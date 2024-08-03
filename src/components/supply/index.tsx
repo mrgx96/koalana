4847837023ca2f85c19d404b69b2d8ac9e82c0a5
dragon-8800 <159820590+dragon-8800@users.noreply.github.com>
2024-08-03 11:49:33 -0400

    :lipstick: fix: remove middle line on supply

--- src/components/supply/index.tsx
+++ src/components/supply/index.tsx
@@ -7,7 +7,7 @@ const Supply = () => {
       <h3>Total Supply: 1 billion</h3>
       <div className={style.progressBars}>
         <ProgressBar color="#D32B85" label="100% community token created on pump.fun" value={100} />
-        <ProgressBar color="#FFAD00" label="150M-200M tokens burned for a deflationary boost" value={20} />
+        {/* <ProgressBar color="#FFAD00" label="150M-200M tokens burned for a deflationary boost" value={20} /> */}
         <ProgressBar color="#28b3b4" label="All liquidity is locked by burning the LP token" value={100} />
       </div>
     </section>
